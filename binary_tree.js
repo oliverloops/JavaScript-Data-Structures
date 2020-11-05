0d41d4ae069f4ee0eb324d6d70ff1f2e86a0e884
oliverloops <oliveralr@protonmail.com>
2020-11-04 18:41:44 -0600

    new mods

--- binary_tree.js
+++ binary_tree.js
@@ -1,17 +1,28 @@
 class Arboles {
   generator() {
+    //Crear propio árbol binario
+    const ap = new NodoArbol("p", null, null);
+    const bp = new NodoArbol("p", null, null);
+    const ah3 = new NodoArbol("h3", null, null);
+    const bh3 = new NodoArbol("h3", null, null);
+    const aDiv = new NodoArbol("div", ah3, ap);
+    const bDiv = new NodoArbol("div", bh3, bp);
+    const body = new NodoArbol("body", aDiv, bDiv);
+
     //Probar los 3 recorridos
     const c = new NodoArbol("Pan", null, null);
     const a = new NodoArbol("Gohan", c, null);
     const b = new NodoArbol("Goten", null, null);
     const root = new NodoArbol("Goku", a, b);
 
-    /*this.recorridoPreOrden(root);
-    this.recorridoEnOrden(root);
-    this.recorridoPostOrden(root);*/
+    this.recorridoPreOrden(body);
+    this.recorridoEnOrden(body);
+    this.recorridoPostOrden(body);
 
     //Probar las 3 búsquedas
-    this.buscarPreOrden(root, "Goku");
+    this.buscarPreOrden(body, "div");
+    this.buscarEnOrden(aDiv, "p");
+    this.buscarPostOrden(aDiv, "h3");
   }
 
   //Recorridos
@@ -53,6 +64,42 @@ class Arboles {
       }
     }
     return null;
+  }
+
+  buscarEnOrden(node, busqueda) {
+    if (node.arbolIzq !== null) {
+      let encontrado = this.buscarPreOrden(node.arbolIzq, busqueda);
+      if (encontrado !== null) {
+        return encontrado;
+      }
+    }
+    if (node.dato === busqueda) {
+      return node;
+    }
+    if (node.arbolDer !== null) {
+      let encontrado = this.buscarPreOrden(node.arbolDer, busqueda);
+      if (encontrado !== null) {
+        return encontrado;
+      }
+    }
+  }
+
+  buscarPostOrden(node, busqueda) {
+    if (node.arbolIzq !== null) {
+      let encontrado = this.buscarPreOrden(node.arbolIzq, busqueda);
+      if (encontrado !== null) {
+        return encontrado;
+      }
+    }
+    if (node.arbolDer !== null) {
+      let encontrado = this.buscarPreOrden(node.arbolDer, busqueda);
+      if (encontrado !== null) {
+        return encontrado;
+      }
+    }
+    if (node.dato === busqueda) {
+      return node;
+    }
   }
 }
 
